a5f4a61031c7be5193637b5d2a7b166d3e4c60f4
Ram-gill <65101581+Ram-Moudgill@users.noreply.github.com>
2021-08-24 18:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let doc=document.getElementById("cards");
 doc.style.display="none";
-document.queryselector("#enterValue").addEventListener('input',(e)=>
+document.querySelector("#enterValue").addEventListener('input',(e)=>
 {
      doc.style.display="block";
     console.log("something entered succesfully");
     let value=e.target.value;
-    document.queryselector("#grams").innerHTML=`Grams: ${value/0.0022046} gms.`;
-    document.queryselector("#kgs").innerHTML=`Kg: ${value*0.45359237} kgs.`;
-    document.queryselector("#tan").innerHTML=`tonne: ${value*0.00045359237} ts.`;
+    document.querySelector("#grams").innerHTML=`Grams: ${value/0.0022046} gms.`;
+    document.querySelector("#kgs").innerHTML=`Kg: ${value*0.45359237} kgs.`;
+    document.querySelector("#tan").innerHTML=`tonne: ${value*0.00045359237} ts.`;
 })
